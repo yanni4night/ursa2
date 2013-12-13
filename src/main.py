d6c5438d883e98733f4c57020cb3e1c67b461ed8
yinyong <yanni4night@gmail.com>
2013-12-13 23:38:19 +0800

    支持init目录

--- src/main.py
+++ src/main.py
@@ -12,12 +12,32 @@
  @since 0.0.1
 '''
 
-from conf  import log,C
+from conf  import log,C,BASE_DIR
 from docopt import docopt
 from __init__ import __version__ as version
-import sys,os
+import sys
+import os
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+
+def copyfiles(sourceDir, targetDir):
+    """拷贝文件夹
+    
+    Arguments:
+    - `filename`:
+    """
+    for file in os.listdir(sourceDir): 
+        sourceFile = os.path.join(sourceDir,  file) 
+        targetFile = os.path.join(targetDir,  file) 
+        if os.path.isfile(sourceFile): 
+            if not os.path.exists(targetDir):  
+                os.makedirs(targetDir)  
+            if not os.path.exists(targetFile) or(os.path.exists(targetFile) and (os.path.getsize(targetFile) != os.path.getsize(sourceFile))):  
+                open(targetFile, "wb").write(open(sourceFile, "rb").read()) 
+        if os.path.isdir(sourceFile): 
+            copyfiles(sourceFile, targetFile)  
+
 
 def run():
     '''
@@ -37,8 +57,15 @@ def run():
     '''
     argv=docopt(run.__doc__,version=version)
     if argv.get('init'):
-        #todo
-        print 'coming'
+        if os.listdir( '.' ):
+            log.warn('Not an empty folder.\nContinue may change your exists files.\nStill Continue?')
+            iscontinue = raw_input('(y/n):').lower() in ['y' , 'yes','ok' ]
+            if not iscontinue:
+                log.info('User cancel init.')
+                sys.exit(1)
+        log.info('Begin to init current folder')
+        copyfiles(os.path.join(BASE_DIR,'../assets','clo'),'.')
+        log.info('Init success.')
     elif argv.get('start'):
         server=__import__('server')
         server.run(argv.get('<port>'))
